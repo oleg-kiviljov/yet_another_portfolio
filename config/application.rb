b90df534b1386f92e74dbfefde59e75e316320b7
OLEG KIVILJOV <oleg.kiviljov@gmail.com>
2015-09-07 00:16:25 +0200

    remove ActiveRecord

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,7 +24,7 @@ module Portfolio
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
     # Mailer
     config.action_mailer.delivery_method = :smtp
